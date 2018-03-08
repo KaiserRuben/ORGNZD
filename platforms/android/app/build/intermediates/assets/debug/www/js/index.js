051e095038f562e21ba04d050751fd9691cd6369
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-08 19:26:01 +0100

    added Lennards server

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -10,7 +10,7 @@ var app = {
         app.receivedEvent('deviceready');
 
         // Here, we redirect to the web site.
-        var targetUrl = "http://192.168.0.157/ORGNZD-site";
+        var targetUrl = "https://orgnzd.lennartvonwerder.de/";
         var bkpLink = document.getElementById("bkpLink");
         bkpLink.setAttribute("href", targetUrl);
         bkpLink.text = targetUrl;
