a484e1cf705aab6f75cf2136b1c12c97cc2ac710
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-08 16:06:13 +0100

    first working app-stuckture

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -10,7 +10,7 @@ var app = {
         app.receivedEvent('deviceready');
 
         // Here, we redirect to the web site.
-        var targetUrl = "https://YOUR-HOSTED-WEB-APP-URL/";
+        var targetUrl = "https://google.de";
         var bkpLink = document.getElementById("bkpLink");
         bkpLink.setAttribute("href", targetUrl);
         bkpLink.text = targetUrl;
