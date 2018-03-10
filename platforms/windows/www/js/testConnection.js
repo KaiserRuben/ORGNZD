aae131687c4fa181c12b6d022897d31387c5469a
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-10 19:37:06 +0100

    version 0.1.4

---
+++ platforms/windows/www/js/testConnection.js
@@ -0,0 +1,7 @@
+﻿console.log(navigator.onLine);
+if(navigator.onLine){
+    var weiterleitung = 'https://192.168.0.4:8000';
+    window.setTimeout('window.location = weiterleitung',0);
+}else{
+  document.write('Bitte gehe online, um die App zu benutzen.');
+}
--- www/js/testConnection.js
+++ www/js/testConnection.js
@@ -1,6 +1,6 @@
 console.log(navigator.onLine);
 if(navigator.onLine){
-    var weiterleitung = 'https://orgnzd.lennartvonwerder.de/';
+    var weiterleitung = 'https://192.168.0.4:8000';
     window.setTimeout('window.location = weiterleitung',0);
 }else{
   document.write('Bitte gehe online, um die App zu benutzen.');
