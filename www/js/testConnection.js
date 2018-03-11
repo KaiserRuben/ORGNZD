ab7c52c11aad87b9ba823f118174a8ec6f428c92
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-11 11:18:19 +0100

    added nice loading message

--- www/js/testConnection.js
+++ www/js/testConnection.js
@@ -1,5 +1,6 @@
 console.log(navigator.onLine);
 if(navigator.onLine){
+    document.write('Please waite a second to get (to) ORGNZD.');
     var weiterleitung = 'https://orgnzd-new.herokuapp.com/';
     window.setTimeout('window.location = weiterleitung',0);
 }else{
