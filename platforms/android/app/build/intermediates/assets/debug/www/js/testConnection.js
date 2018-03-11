a8a118168e999d834b7fb15b486147f13e08b2c7
Ruben Kaiser <C:\Users\Ruben\AppData\Roaming\The Bat!>
2018-03-11 10:52:45 +0100

    added splashscreen

--- platforms/android/app/build/intermediates/assets/debug/www/js/testConnection.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/testConnection.js
@@ -1,6 +1,6 @@
 console.log(navigator.onLine);
 if(navigator.onLine){
-    var weiterleitung = 'https://192.168.0.4:8000';
+    var weiterleitung = 'https://orgnzd-new.herokuapp.com/';
     window.setTimeout('window.location = weiterleitung',0);
 }else{
   document.write('Bitte gehe online, um die App zu benutzen.');
